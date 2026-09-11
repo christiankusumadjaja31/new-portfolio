0f98718cbe92a36a388a9fdefd65b4810c530273
christiankusumadjaja31 <christiankusumadjaja31@gmail.com>
2026-09-12 02:57:53 +0700

    feat: fix base link

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://christiankusumadjaja31.github.io',
-  base: '/portfolio', // Ganti 'portfolio' dengan nama repositori GitHub-mu nanti
+  base: '/new-portfolio', // Ganti 'portfolio' dengan nama repositori GitHub-mu nanti
 });
 
 
