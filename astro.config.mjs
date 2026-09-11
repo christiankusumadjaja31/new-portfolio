2bac25741ade88509ba51df90f44d301f8e5ecec
christiankusumadjaja31 <christiankusumadjaja31@gmail.com>
2026-09-12 02:51:07 +0700

    feat: setup astro deployment for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://christiankusumadjaja31.github.io',
+  base: '/portfolio', // Ganti 'portfolio' dengan nama repositori GitHub-mu nanti
+});
+
+
